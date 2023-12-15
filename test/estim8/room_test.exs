cbcf6e7f68d3a52b9b49368a8b4a333a82a47e67
sesamzoo <16018808+sesamzoo@users.noreply.github.com>
2023-12-15 22:29:42 +0100

    Fix room test

--- test/estim8/room_test.exs
+++ test/estim8/room_test.exs
@@ -8,18 +8,23 @@ defmodule Estim8.RoomRegistryTest do
 
   test "join_or_create_and_join creates new room", _ do
     test_room_id = "room_id"
-    test_user = %{id: "id", name: "name"}
+    test_user = %{id: "id", name: "name", card: nil}
     room = Estim8.RoomRegistry.join_or_create_and_join(test_room_id, test_user)
     room_data = Estim8.Room.get(room)
-    assert room_data.users == [test_user]
 
-    test_user2 = %{id: "id2", name: "name2"}
+    assert map_size(room_data.users) == 1
+    assert Map.get(room_data.users, test_user.id) == test_user
+
+    test_user2 = %{id: "id2", name: "name2", card: nil}
     room = Estim8.RoomRegistry.join_or_create_and_join(test_room_id, test_user2)
     room_data = Estim8.Room.get(room)
-    assert room_data.users == [test_user2, test_user]
+    assert map_size(room_data.users) == 2
+    assert Map.get(room_data.users, test_user.id) == test_user
+    assert Map.get(room_data.users, test_user2.id) == test_user2
 
-    Estim8.Room.leave(room, test_user.id)
+    Estim8.Room.leave(room, test_user)
     room_data = Estim8.Room.get(room)
-    assert room_data.users == [test_user2]
+    assert map_size(room_data.users) == 1
+    assert Map.get(room_data.users, test_user2.id) == test_user2
   end
 end
